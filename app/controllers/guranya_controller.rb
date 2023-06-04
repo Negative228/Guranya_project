4a593adaadc47352d692305623a05e0ba58731f8
TaylbTabe <taylbtabe@gmail.com>
2023-06-04 22:59:08 +0300

    koshki shutki 2
    xd

--- app/controllers/guranya_controller.rb
+++ app/controllers/guranya_controller.rb
@@ -1,5 +1,42 @@
 class GuranyaController < ApplicationController
+  include GuranyaHelper
+  
   def make_joke
-    @response = HTTParty.get("https://api.publicapis.org/categories")
+    j_type = params["name"]
+    if (valid_joke?(j_type))
+      joke = HTTParty.get('https://official-joke-api.appspot.com/jokes/' + j_type.to_s + '/random')
+      @response = JSON.parse(joke.body)[0]["setup"] + "  " + JSON.parse(joke.body)[0]["punchline"]
+    else
+    redirect_to :main
+    end
   end
+
+  def num_fact
+    num = params["name"]
+    if (valid_num?(num))
+      number = HTTParty.get('http://numbersapi.com/' + num.to_s + '/')
+      @response = number.body
+    else
+      redirect_to :main
+    end
+  end
+
+  def cat_breed
+    breed = params["name"]
+    breed = breed.capitalize()
+    if (valid_cat?(breed))
+      cat = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}",
+        headers: {'X-Api-Key': 'wCwPBOjfVh4Sd6+wk5qlag==iTrhbDSMz9f3ppXh'})
+       @response = JSON.parse(cat.body)[0]["image_link"]
+    else
+      redirect_to :main
+    end
+  end
+
+
+
+  def main
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'guranya#make_joke'
+  root 'guranya#main'
+  get '/make_joke', to: 'guranya#make_joke'
+  get '/num_fact', to: 'guranya#num_fact'
+  get '/cat_breed', to: 'guranya#cat_breed'
+  get '/main', to: 'guranya#main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
