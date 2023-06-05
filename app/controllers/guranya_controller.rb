a2f047e3e61c0ac3a3afec74132a4687f132abcd
Askobarus <salnikov.salnikov2010@yandex.ru>
2023-06-05 14:27:34 +0300

    database created + ui updated

--- app/controllers/guranya_controller.rb
+++ app/controllers/guranya_controller.rb
@@ -35,6 +35,7 @@ class GuranyaController < ApplicationController
 
 
   def main
+    @breeds = Breed.all
   end
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -67,4 +68,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "6de0-213-27-22-158.ngrok-free.app"
 end
